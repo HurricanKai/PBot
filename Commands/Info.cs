5d6afd11c535f83c35dcfda78da328cfd6ad7871
HurricanKai <herrucan.schmitt@gmail.com>
2018-03-17 10:51:23 +0100

    Add Sample Command

---
+++ Commands/Info.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBot.Commands
+{
+    public class Info : BaseCommandModule
+    {
+		[Command("Info")]
+		public async Task InfoCMD(CommandContext ctx)
+		{
+			var embed = new DiscordEmbedBuilder();
+            embed.WithColor(DiscordColor.Cyan);
+            embed.WithTitle("Info");
+            embed.AddField("Name", "Some Name");
+            embed.AddField("Author", "romangraef89ಜ್ಞಾ#0998 & HurricanKai#9999");
+
+            await ctx.RespondAsync(embed: embed);
+		}
+    }
+}
