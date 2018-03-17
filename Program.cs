389c1fafd9b50e9960f6407eed48171f79a44e34
HurricanKai <herrucan.schmitt@gmail.com>
2018-03-17 10:47:32 +0100

    Init Bot

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Bot b = new Bot();
+			b.RunAsync().Wait();
+        }
+    }
+}
